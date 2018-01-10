b487ec8f71719b2a13ac7a8f6472100c12aee779
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-10 16:40:06 +0530

    Create letter.c

---
+++ letter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+{
+	int a;
+	int i=a;
+	printf(" enter the given  letter ");
+	scanf("d",&i);
+	if(i==a)
+	{
+		printf("enter the letter as alphabet");
+	}
+	else
+	{
+		printf(" enter the  given word is not a letter ");
+	}
+}
