90b93b3ba9803b9dce76d01d5bca0b3f4e960620
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-10 17:01:43 +0530

    Create largest number .c

---
+++ largest number .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i=1;
+	printf("enter the given number");
+	scanf("%d,&i");
+	if(i=1;i<=3;i++)
+	{
+		printf(" the largest number is");
+	}
+	else
+	{
+		printf(" the given number is not a largest number ");
+	}
+}
+
