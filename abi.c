7795e7984ee48501a824836d674834a9d598b3f4
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-10 15:59:47 +0530

    Create abi.c

---
+++ abi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number ");
+scanf("%d",&a);
+if(a>0)
+{
+printf("given number is positive");
+}
+if(a<0)
+{
+printf("given number is negative");
+}
+else
+{
+printf(" the given number is equal to zero");
+}
+}
+
