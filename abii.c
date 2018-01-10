fa265f5122d52ed27e447b8736a58a6f9438939e
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-10 16:17:22 +0530

    Create abii.c

---
+++ abii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a=3;
+printf("enter the number");
+scanf("%d",&a);
+if(a==3)
+{
+	printf("enter the given number is odd");
+}
+else
+{
+	printf("enter the given number is even");
+}
+}
