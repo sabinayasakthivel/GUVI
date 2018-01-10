6ee6377f2f1132fdb50903f3c66de11b6d788e70
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-10 16:32:41 +0530

    Create vow r  conso.c

---
+++ vow r  conso.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+{
+	int a;
+	int i=a;
+	printf(" enter the given alphapet letter ");
+	scanf("d",&i);
+	if(i==a)
+	{
+		printf("enter the alphapet as vowel");
+	}
+	else
+	{
+		printf("enter the alphaprt as consonant");
+	}
+}
